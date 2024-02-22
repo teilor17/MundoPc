5b49d74c1e076fe4059a5286b2354fff63a7cb5a
Tailor <teilorza17@gmail.com>
2024-02-22 11:49:35 -0500

    finalizacion de la clase computador 1

--- Computador.py
+++ Computador.py
@@ -3,6 +3,8 @@ class Computador:
     id_computadora = 0
 
     def __init__(self, nombre, monitor, teclado, raton):
+        Computador.id_computadora += 1
+        Computador.contador_computadoras += 1
         self._nombre = nombre
         self._monitor = monitor
         self._teclado = teclado
@@ -24,4 +26,22 @@ class Computador:
     def raton(self):
         return self._raton
 
-    @getattr()
+    @nombre.setter
+    def nombre(self, nombre):
+        self._nombre = nombre
+
+    @monitor.setter
+    def nombre(self, monitor):
+        self._monitor = monitor
+
+    @teclado.setter
+    def nombre(self, teclado):
+        self._teclado = teclado
+
+    @nombre.setter
+    def raton(self, raton):
+        self._raton = raton
+
+    def __str__(self):
+        return f'Id : {Computador.id_computadora}, \n Nombre : {self._nombre}'
+
