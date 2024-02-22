14aca4546aaec1bc5a77e59eab28509c38c82bd3
Tailor <teilorza17@gmail.com>
2024-02-22 13:11:22 -0500

    correcion clase teclado

--- Teclado.py
+++ Teclado.py
@@ -1,9 +1,12 @@
 from DispositivoEntreda import DispoEntrada
 class Teclado(DispoEntrada):
     contador_teclado = 0
-    id_teclado = 0
     def __init__(self,tipoEntrada, marca):
+        Teclado.contador_teclado +=1
+        self._id_teclado = Teclado.contador_teclado
         super().__init__(tipoEntrada, marca)
 
+
     def __str__(self):
-        return
+        return f'Id : {self._id_teclado}, Marca : {self._marca}, Tipo entrada : {self._tipoEntrada}'
+
