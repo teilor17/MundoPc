42067cefdbbcdf32825eccd293882deba0339505
Tailor <teilorza17@gmail.com>
2024-02-22 13:10:38 -0500

    correcion de clase raton

--- Raton.py
+++ Raton.py
@@ -1,10 +1,12 @@
 from DispositivoEntreda import DispoEntrada
 class Raton(DispoEntrada):
     contador_ratones = 0
-    id_raton = 0
     def __init__(self,tipoEntrada, marca):
+        Raton.contador_ratones +=1
+        super().__init__(tipoEntrada, marca)
+        self._id_raton = Raton.contador_ratones
         super().__init__(tipoEntrada, marca)
 
     def __str__(self):
-        return
+        return f'Id : {self._id_raton}, Marca : {self._marca}, Tipo entrada : {self._tipoEntrada}'
 
